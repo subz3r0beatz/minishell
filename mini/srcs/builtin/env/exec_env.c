67f0d5fe91eadea2b212a0c4f4930ba5522cc948
subz3r0 <flavio.dumas@outlook.es>
2026-07-11 12:27:37 +0200

    update exec_env

--- mini/srcs/builtin/env/exec_env.c
+++ mini/srcs/builtin/env/exec_env.c
@@ -1,17 +1,32 @@
 #include "minishell.h"
 #include <stdlib.h>
 
-static int	switch_dir(char	*path)
+static void	switch_dir(char	*path)
 {
 	if (path && chdir(path))
 	{
 		ft_putstr_fd("minishell: env: cannot change directory to '", STDERR_FILENO);
 		ft_putstr_fd(path, STDERR_FILENO);
-		ft_putstr_fd(', STDERR_FILENO);
+		ft_putstr_fd("'", STDERR_FILENO);
 		perror();
 		exit(125);
 	}
 }
+
+static int	resolve_path(char **args, char **exported, t_flags *flags)
+{
+	char	*cmd;
+	char	*path;
+
+	cmd = args[0];
+	if (ft_strchr(cmd, '/'))
+	{
+		
+	}
+	if (get_)
+}
+
+static void	exec_cmd(char **args, char **exported, t_flags *flags)
 
 int	exec_env(char	**args, char **exported, t_flags *flags)
 {
@@ -27,6 +42,7 @@ int	exec_env(char	**args, char **exported, t_flags *flags)
 	if (pid == 0)
 	{
 		switch_dir(flags->chdir_path);
+		exec_cmd(args, exported, flags);
 	}
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
